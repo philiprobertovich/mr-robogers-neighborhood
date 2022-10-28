9f3e7415459510305989b22dc9543a8b1b8250b6
Phil Kendall <prkkendall@gmail.com>
2022-10-28 11:18:50 -0700

    finish the structure of the beepBoop function to be able to execute the next test.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ function beepBoop(input) {
   let numArray = [];
   for (i = 0; i <= input; i++) {
     numArray.push(i);
+    let iSplitArray = [numArray[i]];
+    iSplitArray.split('');
+    iSplitArray.forEach(if(element === 1) {
+      iSplitArray = "Beep!";
+      iSplitArray = numArray[i];
+    }
+    else {
+      iSplitArray.join();
+      iSplitArray = numArray;
+    }
   }
   return numArray;
 }
