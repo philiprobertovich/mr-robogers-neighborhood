55507b14491094ea68f8bc20ca248f87104fadc3
Phil Kendall <prkkendall@gmail.com>
2022-10-28 14:16:19 -0700

    struggling with .forEach bug

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,32 @@ function beepBoop(input) {
   let numArray = [];
   for (i = 0; i <= input; i++) {
     numArray.push(i);
-    if(numArray[i] === 1) {
-      numArray[i] = "Beep!";
-    } 
-    else if (numArray[i] === 2) {
-      numArray[i] = "Boop!";
+    if (i < 10) {
+      if(numArray[i] === 1) {
+        numArray[i] = "Beep!";
+      } 
+      else if (numArray[i] === 2) {
+        numArray[i] = "Boop!";
+      }
+      else if (numArray[i] === 3) {
+        numArray[i] = "Won't you be my neighbor?";
+      }
     }
-    else if (numArray[i] === 3) {
-      numArray[i] = "Won't you be my neighbor?";
+    else {
+      let iSplitArray = [];
+      iSplitArray.push(numArray[i].toString());
+      console.log(iSplitArray);
+      iSplitArray.split('');
+      console.log(iSplitArray);
+      iSplitArray.forEach(function(element) {
+        if(element === "1") {
+          iSplitArray = "Beep!";
+          iSplitArray = numArray[i];
+        }
+      });
     }
   }
   return numArray;
 }
-
-let iSplitArray = [numArray[i]];
-iSplitArray.split('');
-    iSplitArray.forEach(function(element) {
-      if(element === 1) {
-        iSplitArray = "Beep!";
-        iSplitArray = numArray[i];
-      }
-      else {
-        iSplitArray.join();
-        iSplitArray = numArray[i];
-      }
-    });
-
-
 
 // UI logic
