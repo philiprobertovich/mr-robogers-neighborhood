e68d2258aad1af1d95996857b77db6d780ac3b10
Phil Kendall <prkkendall@gmail.com>
2022-10-28 11:48:19 -0700

    pass the current test.

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,25 @@ function beepBoop(input) {
   let numArray = [];
   for (i = 0; i <= input; i++) {
     numArray.push(i);
-    let iSplitArray = [numArray[i]];
-    iSplitArray.split('');
-    iSplitArray.forEach(if(element === 1) {
-      iSplitArray = "Beep!";
-      iSplitArray = numArray[i];
-    }
-    else {
-      iSplitArray.join();
-      iSplitArray = numArray;
-    }
+    if(numArray[i] === 1) {
+      numArray[i] = "Beep!";
+    } 
   }
   return numArray;
 }
+
+let iSplitArray = [numArray[i]];
+iSplitArray.split('');
+    iSplitArray.forEach(function(element) {
+      if(element === 1) {
+        iSplitArray = "Beep!";
+        iSplitArray = numArray[i];
+      }
+      else {
+        iSplitArray.join();
+        iSplitArray = numArray[i];
+      }
+    });
 
 
 
